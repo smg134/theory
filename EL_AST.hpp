1e33ed37c39893c7cde689ecd88a40e20193cdf5
Sean Gaffney <smg134@zips.uakron.edu>
2017-09-09 20:35:30 -0400

    Added primitive domains

---
+++ EL_AST.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+//Integer literal
+//int
+
+//Boolean Literal
+//bool
+
+//Arithmetic operator
+enum arith_op {
+	add,
+	sub,
+	mul,
+	div,
+	mod,
+};
+
+//Relational operator
+enum rel_op {
+	less,
+	equal,
+	greater,
+};
+
+//Logic operator
+enum logic_op {
+	and,
+	or,
+};
+
