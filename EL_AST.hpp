4937e86002c2634ca52691827527418bfe6be726
Sean Gaffney <smg134@zips.uakron.edu>
2017-09-09 20:50:06 -0400

    Added program and sorta num_expr/bool_expr

--- EL_AST.hpp
+++ EL_AST.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+struct Program;
+struct num_expr;
+struct bool_expr;
+
 //Integer literal
 //int
 
@@ -28,3 +32,18 @@ enum logic_op {
 	or,
 };
 
+//Program
+struct Program {
+	int args;
+	num_expr body;
+};
+
+//Numeric expression
+struct num_expr {
+
+};
+
+//Boolean expression
+struct bool_expr {
+
+};
